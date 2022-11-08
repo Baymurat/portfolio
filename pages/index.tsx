0182cf7769cba086157ce457fe5e4bb8518c9dcc
Baymurat <baymurat.orazov@mail.ru>
2022-11-08 20:08:57 +0500

    fetch data to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import About from "../components/About";
@@ -8,8 +8,28 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchExperience } from "../utils/fetchExperiences";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchSocial } from "../utils/fetchSocials";
 
-const Home: NextPage = () => (
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+}
+
+const Home = ({
+  pageInfo,
+  experiences,
+  skills,
+  projects,
+  socials,
+}: Props) => (
   <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory scroll-smooth
    overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
     <Head>
@@ -59,3 +79,22 @@ const Home: NextPage = () => (
 );
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperience();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocial();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  };
+};
